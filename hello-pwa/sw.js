5ff239e9b97107b2c7e0d85d1f417e0c17bd420d
Dick Heijink <38525231+J-D-Heijink@users.noreply.github.com>
2021-08-31 13:48:58 +0200

    Update sw.js

--- hello-pwa/sw.js
+++ hello-pwa/sw.js
@@ -18,6 +18,7 @@ self.addEventListener('install', function(e) {
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
+  console.log(e);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
